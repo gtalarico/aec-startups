4ec010d6387e5481d868a2397669dfaefb3e61f6
gtalarico <gtalarico.dev@gmail.com>
2019-06-06 00:49:45 -0700

    Removed content-length

--- lambda/airtable.js
+++ lambda/airtable.js
@@ -1,11 +1,6 @@
-const zlib = require('zlib')
 const Airtable = require('airtable')
 
 exports.handler = function(event, context, callback) {
-  // const acceptsGzip = event.headers['Accept-Encoding']
-  //   ? event.headers['Accept-Encoding'].contains('gzip')
-  //   : false
-  const acceptsGzip = false
 
   Airtable.configure({
     endpointUrl: 'https://api.airtable.com',
@@ -31,19 +26,7 @@ exports.handler = function(event, context, callback) {
           callback(err)
         } else {
           const body = JSON.stringify({ records: allRecords })
-          if (!acceptsGzip) {
-            // Send string response
             sendResponse(callback, body, false)
-          } else {
-            // Send gzipped response
-            zlib.gzip(body, function(gzipErr, gzippedBody) {
-              if (gzipErr) {
-                callback(gzipErr)
-              } else {
-                sendResponse(callback, gzippedBody, true)
-              }
-            })
-          }
         }
       }
     )
@@ -56,12 +39,7 @@ function sendResponse(callback, body, gzip) {
     headers: {
       'content-type': 'application/json',
       'cache-control': 'Cache-Control: max-age=300, public',
-      'content-length': Buffer.from(body).length
     }
-  }
-  if (gzip) {
-    response.isBase64Encoded = true
-    response.headers['content-encoding'] = 'gzip'
   }
   callback(null, response)
 }
