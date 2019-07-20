d46b937ec50084ab8e6c632c00e7ca2c7c3853b7
gtalarico <gtalarico.dev@gmail.com>
2019-07-20 12:05:24 -0700

    Fixes flex in mobile

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -4,7 +4,7 @@
     :class="{ 'box-entry-selected': $route.hash.includes(cardId) }"
     @click="$emit('click')"
   >
-    <div class="d-flex flex-row align-items-top">
+    <div class="d-flex flex-wrap flex-row align-items-top">
       <div class="logo d-flex align-items-center justify-content-center">
         <div v-show="!imageReady" class="spinner-grow" role="status">
           <span class="sr-only">Loading...</span>
@@ -79,36 +79,39 @@ export default {
 </script>
 
 <style lang="scss">
+@import '@/assets/scss/app.scss';
+
 .box-entry {
-  margin: 0 0 1.5rem 1.5rem;
   display: block;
+  font-size: 0.75rem;
   border: 1px solid #dedede;
   border-radius: 4px;
   overflow: hidden;
-  font-size: 0.75rem;
-
   box-shadow: 0 4px 6px 0 rgba(0, 0, 0, 0.1);
   background-color: white;
-  .info-box {
-    padding: 0.5rem 0.75rem;
-  }
-  .details-box {
-    padding-top: 0.5rem;
-    padding-right: 0.5rem;
-  }
   &:hover,
   &.box-entry-selected {
     border-color: #999;
     box-shadow: 1px 5px 7px 0 rgba(0, 0, 0, 0.2);
   }
 
+  // desktop
+  @include media-breakpoint-up(sm) {
+    margin: 0 0 1.5rem 1.5rem;
+  }
+  margin-top: 1.5rem;
+
   a {
     font-weight: 400;
   }
 
   .logo {
-    width: 13rem;
-    height: 11rem;
+    @include media-breakpoint-up(sm) {
+      width: 13rem;
+      height: 11rem;
+    }
+    width: 30rem;
+    height: 10rem;
     img {
       width: 100%;
       height: 100%;
@@ -123,7 +126,13 @@ export default {
   }
 
   .info-box {
-    width: 16rem;
+    padding: 0.75rem 0.75rem;
+
+    width: 22rem;
+    @include media-breakpoint-up(sm) {
+      width: 16rem;
+      padding-top: 0.5rem;
+    }
 
     .title {
       font-size: 1rem;
@@ -143,14 +152,17 @@ export default {
   }
 
   .details-box {
+    @include media-breakpoint-up(sm) {
+      padding-left: 0;
+    }
+    padding-left: 0.75rem;
+    padding-top: 0.5rem;
+    padding-right: 0.75rem;
     width: 6rem;
 
     ul {
       list-style: none;
       padding-left: 0;
-      li {
-        // padding-left: 0.75rem;
-      }
     }
   }
 
